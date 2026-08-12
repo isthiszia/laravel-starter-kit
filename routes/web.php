5313d187981c653e7c1d8999834eb7bc5e001edf
Zia <ziabangash293@gmail.com>
2026-08-13 04:56:24 +0500

    show home welcome screen to login page

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome')->name('home');
+Route::get('/', function () {
+    return redirect()->route('login');
+})->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
